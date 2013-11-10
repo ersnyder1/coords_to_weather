9de2ba8b5f1f0d5a4db805ca4c1e1777df4259f8
Eric Snyder <ersnyder1@gmail.com>
2013-11-10 17:50:40 -0600

    Completed challenge
    Completed challenge (re-syncing this time, because it didn't sync
    correctly the first time). App defaults to showing weather for Kellogg
    until another address is entered.

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,17 +3,28 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
-    your_api_key = ""
+    
+    if (val=params[:address]).blank? == true
+        @address = "Corner of Foster and Sheridan"
+    else
+    @address = params[:address]
+    end
 
-    # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @temperature = ?
-    # @minutely_summary = ?
-    # @hourly_summary = ?
-    # @daily_summary = ?
+     @url_safe_address = URI.encode(@address)
+    url_a = "http://maps.googleapis.com/maps/api/geocode/json?address="+ @url_safe_address +"&sensor=false"
+    raw_data = open(url_a).read
+    parsed_data = JSON.parse(raw_data)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    @your_api_key = "1da527e2fe88c3b347ef24bc4348aa72"
+
+ 
+     url_b = "https://api.forecast.io/forecast/" + @your_api_key + "/" + @latitude.to_s+ "," + @longitude.to_s
+     raw_data = open(url_b).read
+     parsed_data = JSON.parse(raw_data)
+     @temperature = parsed_data["currently"]["temperature"]
+     @minutely_summary = parsed_data["minutely"]["summary"]
+     @hourly_summary = parsed_data["hourly"]["summary"]
+     @daily_summary = parsed_data["daily"]["summary"]
   end
 end
